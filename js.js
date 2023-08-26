36f474f32f8a9e402b22744cc57a4a387c618530
Moin Hasanfatta <112717999+MoinHasanfatta@users.noreply.github.com>
2023-08-26 11:48:25 +0530

    Add files via upload
    Basic project in JBK on Js

---
+++ js.js
@@ -0,0 +1,28 @@
+// abc = "I am coding"
+
+// console.log(abc)
+// console.log('abc')
+// console.log("2*4");
+// x = 5
+// y =4
+// z = (x- y)
+// z1 = (y * x)
+// console.log(z)
+// console.log(z1
+
+
+let text = document.querySelector('.jbk')
+console.log (text)
+let xyz = document.getElementById('btn')
+console.log(xyz)
+
+// text.innerHTML ="not interested"
+
+function okay(){
+    if(text.innerHTML == "This is a text"){
+        text.innerHTML = "No text required"
+    }
+    else{text.innerHTML = "This is a text"}
+    
+
+}
